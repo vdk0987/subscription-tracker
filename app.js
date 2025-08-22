5764b067c30045404f062487518318f64776d9c8
vdk0987 <vdk0987@gmail.com>
2025-08-22 18:54:31 +0530

    add error handling middleware

--- app.js
+++ app.js
@@ -4,12 +4,19 @@ import userRouter from "./routes/user.routes.js";
 import authRouter from "./routes/auth.routes.js";
 import subRouter from "./routes/subscription.routes.js";
 import connectToDatabase from "./database/mongodb.js";
+import errorMiddleware from "./middleware/error.middleware.js";
+import cookieParser from "cookie-parser";
 
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
 
 app.use("/api/auth", authRouter);
 app.use("/api/users", userRouter);
 app.use("/api/subscriptions", subRouter);
+
+app.use(errorMiddleware);
 
 app.get("/", (req, res) => {
   res.send("Test");
